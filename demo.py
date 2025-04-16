5009155e34e10e33b136ded216976572315e6a6a
Asma-javeed <asmiijaveed705@gmail.com>
2025-04-16 13:55:40 +0500

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,29 @@
+# print hello world
+print("hello world")
+# print a number
+x=1
+print(x)
+# input your name
+input("Enter your name:")
+name="asma"
+# input enter any number
+a=input ("enter a number:")
+number=10
+print (a)
+# sum of numbers
+a=6
+b=7
+c=a+b 
+print(c)
+# print array of numbers
+Array=('A',1,2,3,4,5)
+print(Array)
+# print string 
+string=('Asma javeed,Mehak')
+print(string)
+# print tuple
+tuple=('ab','bc','ed')
+print(tuple)
+# list
+fruits= ["Apple" ,"Banana" ,"coconut" ,"grapes"]
+print(fruits)
